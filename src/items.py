1721683d1542d9db948d8871cb9808de1a77d5d1
Daluxea <73659254+dddddavid02@users.noreply.github.com>
2021-11-14 15:59:34 +0100

    Applied some improvements

--- src/items.py
+++ src/items.py
@@ -50,7 +50,7 @@ def generate_trans_item(translation, original, from_lang, to_lang):
 def generate_trans_items(translations, from_lang):
     return [
         generate_trans_item(translation, original, from_lang, to_lang)
-    for (translation, original, to_lang) in translations)
+    for (translation, original, to_lang) in translations
     for from_lang in from_lang]
 
 
